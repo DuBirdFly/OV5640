2cda4121ee67a7433a3df335496a4acf84b4bdab
DuBirdFly <2937823748@qq.com>
2022-04-11 17:50:40 +0800

    11

--- matlab/OV5640/top.m
+++ matlab/OV5640/top.m
@@ -6,7 +6,7 @@ clc;
 clear;
 close all;
 
-init_bmp = imread('img\img100x100.bmp'); % 读取图片
+init_bmp = imread('img\img30x30.bmp'); % 读取图片
 sobel_img = sobel_fuc(init_bmp, 100);
 [ram_conn, index_max, conn_rgb] = conn_fuc(sobel_img);
 conn_rgb_line = drawLine(conn_rgb, ram_conn, index_max, [1,0,0], 1);
@@ -17,5 +17,7 @@ subplot(2,3,2); imshow(sobel_img);     title('sobel_{bit}');
 subplot(2,3,3); imshow(conn_rgb);      title('conn_{rgb}');
 subplot(2,3,4); imshow(conn_rgb_line); title('conn_{rgb\_line}');
 subplot(2,3,5); imshow(conn_final);    title('conn_{final}');
+
+sobel_txt_gen(sobel_img);
 
 % con_img_p = drawLine(con_img, ram_conn, index_max, color)
